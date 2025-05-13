3aa2d161859943853f178584b41bfd276cc480ed
Kaitlyn <kaitlynyip8@gmail.com>
2025-05-12 21:34:43 -0700

    Used memory allocation code cuda

--- vecadd_gpu_1t.cu
+++ vecadd_gpu_1t.cu
@@ -1,40 +1,44 @@
 #include <iostream>
-#include <math.h>
-#include <chrono>
+#include <math.h
  
 // Kernel function to add the elements of two arrays
 __global__
-void add(int n, float *sum, float *x, float *y)
+void add(int n, float *x, float *y)
 {
-  for (int i = 0; i < n; i++)
-    sum[i] = x[i] + y[i];
+ for (int i = 0; i < n; i++)
+   y[i] = x[i] + y[i];
 }
  
 int main(void)
 {
- int N = 1<<29; // 512M elements
+ int N = 1<<20;
+ float *x, *y;
  
- // Allocate Unified Memory -- accessible from CPU or GPU
-float *x, *y, *sum;
-cudaMallocManaged(&x, N*sizeof(float));
-cudaMallocManaged(&y, N*sizeof(float));
-
+ // Allocate Unified Memory – accessible from CPU or GPU
+ cudaMallocManaged(&x, N*sizeof(float));
+ cudaMallocManaged(&y, N*sizeof(float));
  
-// Run kernel on 1M elements on the GPU
-add<<<1, 1>>>(N, x, y);
+ // initialize x and y arrays on the host
+ for (int i = 0; i < N; i++) {
+   x[i] = 1.0f;
+   y[i] = 2.0f;
+ }
  
-// Wait for GPU to finish before accessing on host
-cudaDeviceSynchronize();
+ // Run kernel on 1M elements on the GPU
+ add<<<1, 1>>>(N, x, y);
+ 
+ // Wait for GPU to finish before accessing on host
+ cudaDeviceSynchronize();
  
  // Check for errors (all values should be 3.0f)
  float maxError = 0.0f;
- for (int i = 0; i < N; i++)
+ for (int i = 0; i < N; i++) {
    maxError = fmax(maxError, fabs(y[i]-3.0f));
+ }
  std::cout << "Max error: " << maxError << std::endl;
  
-// Free memory
-cudaFree(x);
-cudaFree(y);
- 
- return 0;
+ // Free memory
+ cudaFree(x);
+ cudaFree(y);
+  return 0;
 }
